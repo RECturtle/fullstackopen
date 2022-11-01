62184336c60447e3462bb278341458dd75ca749e
RECturtle <spencer.mertes@gmail.com>
2022-11-01 19:36:38 -0400

    feat: complete examples up to 3.13

--- part3/back/index.js
+++ part3/back/index.js
@@ -1,5 +1,8 @@
+require("dotenv").config()
+
 const cors = require("cors")
 const express = require("express")
+const Note = require("./models/note")
 
 const app = express()
 
@@ -7,6 +10,7 @@ app.use(express.static("build"))
 app.use(express.json())
 app.use(cors())
 
+// Base notes
 let notes = [
 	{
 		id: 1,
@@ -41,50 +45,59 @@ app.get("/", (request, response) => {
 
 // GET - get all notes
 app.get("/api/notes", (request, response) => {
-	response.json(notes)
+	Note.find({}).then((notes) => {
+		response.json(notes)
+	})
 })
 
 // GET - get a note by :id
 app.get("/api/notes/:id", (request, response) => {
-	const id = Number(request.params.id)
-	const note = notes.find((note) => note.id === id)
-
-	// note found
-	if (note) {
+	Note.findById(request.params.id).then((note) => {
 		response.json(note)
-	}
-
-	response.status(404).send("Note not found")
+	})
 })
 
 // POST - add a note
 app.post("/api/notes", (request, response) => {
 	const body = request.body
 
-	if (!body.content) {
-		return response.status(400).json({
-			error: "content missing",
-		})
+	if (body.content === undefined) {
+		return response.status(400).json({ error: "content missing" })
 	}
+
+	const note = new Note({
+		content: body.content,
+		important: body.important || false,
+		date: new Date(),
+	})
+
+	note.save().then((savedNote) => {
+		response.json(savedNote)
+	})
+})
+
+app.put("/api/notes/:id", (request, response) => {
+	const body = request.body
 
 	const note = {
 		content: body.content,
-		important: body.important === true ? true : false,
-		date: new Date(),
-		id: generateId(),
+		important: body.important,
 	}
 
-	notes = notes.concat(note)
-
-	response.json(note)
+	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+		.then((updatedNote) => {
+			response.json(updatedNote)
+		})
+		.catch((error) => console.log(error))
 })
 
 // DELETE - delete a note by :id
 app.delete("/api/notes/:id", (request, response) => {
-	const id = Number(request.params.id)
-	notes = notes.filter((note) => note.id !== id)
-
-	response.status(204).end()
+	Note.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end()
+		})
+		.catch((err) => console.log(err))
 })
 
 const PORT = process.env.PORT || 3001
---
+++ part3/phonebook/backend-phonebook/phoneMongo.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+if (process.argv.length < 3) {
+	console.log(
+		"Please provide the password as an argument: node mongo.js <password> <'contact-name'> <phonenumber>"
+	)
+	process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+const extraArgCheck = process.argv[5] ? true : false
+
+// extra arg checks - guide a user with most likely scenario why this occurred
+if (extraArgCheck) {
+	console.log(
+		"Too many arguments provided, you should add quotes around a contact's name"
+	)
+	process.exit(1)
+}
+
+// needed args check
+if (name && !number) {
+	console.log(
+		"Please provide a name and phonenumber to add a contact or no arguments to retrieve all contacts"
+	)
+	process.exit(1)
+}
+
+// build connection string
+const front_url = process.env.DB_CONNECTION_STRING.slice(0, 26)
+const back_url = process.env.DB_CONNECTION_STRING.slice(26)
+const url = front_url + password + back_url
+
+const contactSchema = new mongoose.Schema({
+	name: String,
+	number: String,
+})
+
+const Contact = mongoose.model("Contact", contactSchema)
+
+function retrieveContacts() {
+	mongoose
+		.connect(url)
+		.then((result) => {
+			console.log("connected")
+
+			console.log("Phonebook:")
+			Contact.find({}).then((result) => {
+				result.forEach((contact) => {
+					console.log(`${contact.name} ${contact.number}`)
+				})
+				mongoose.connection.close()
+			})
+		})
+		.catch((error) => console.log(error))
+}
+
+function createContact() {
+	mongoose
+		.connect(url)
+		.then((result) => {
+			console.log("connected")
+
+			const contact = new Contact({
+				name: name,
+				number,
+			})
+
+			return contact.save()
+		})
+		.then(() => {
+			console.log("===========================================")
+			console.log("contact saved!")
+			console.log(`added ${name} ${number} to phonebook`)
+			console.log("===========================================")
+			return mongoose.connection.close()
+		})
+		.catch((error) => console.log(error))
+}
+
+// only password provided - retrieve users and exit
+if (process.argv.length === 3) {
+	retrieveContacts()
+}
+
+// create password if all args provided
+if (process.argv.length === 5) {
+	createContact()
+}
