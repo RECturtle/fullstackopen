39725e05b7fe6a8b340186351966b210c5e2996b
RECturtle <spencer.mertes@gmail.com>
2022-11-27 18:51:52 -0500

    feat: complete 4.3

--- part4/back/.eslintrc.js
+++ part4/back/.eslintrc.js
@@ -1,27 +1,18 @@
 module.exports = {
-	"env": {
-		"commonjs": true,
-		"es2021": true,
-		"node": true
+	env: {
+		commonjs: true,
+		es2021: true,
+		node: true,
+		jest: true,
 	},
-	"extends": "eslint:recommended",
-	"overrides": [
-	],
-	"parserOptions": {
-		"ecmaVersion": "latest"
+	extends: "eslint:recommended",
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
 	},
-	"rules": {
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"never"
-		]
-	}
+	rules: {
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "never"],
+	},
 }
---
+++ part4/blog/backend-blog/tests/list_helper.test.js
@@ -0,0 +1,7 @@
+const listHelper = require("../utils/list_helper")
+
+test("dummy returns 1", () => {
+    const blogs = []
+
+    expect(listHelper.dummy(blogs)).toBe(1)
+})
