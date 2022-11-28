64b5a0323bc93d48377ab0ca442fb886c831d8d8
RECturtle <spencer.mertes@gmail.com>
2022-11-27 19:59:46 -0500

    feat: complete 4.4

--- part4/blog/backend-blog/utils/list_helper.js
+++ part4/blog/backend-blog/utils/list_helper.js
@@ -1,6 +1,14 @@
 const dummy = (blogs) => {
-    console.log(blogs)
-    return 1
+	console.log(blogs)
+	return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+	const likeReducer = (totalLikes, currLikes) => {
+		return totalLikes + currLikes.likes
+	}
+
+	return blogs.reduce(likeReducer, 0)
+}
+
+module.exports = { dummy, totalLikes }
