f6fac4ec8f0341632622970811ed5ce1435af78a
RECturtle <spencer.mertes@gmail.com>
2023-01-08 19:47:11 -0500

    feat: complete challenges through 4.6

--- part4/blog/backend-blog/utils/list_helper.js
+++ part4/blog/backend-blog/utils/list_helper.js
@@ -11,4 +11,30 @@ const totalLikes = (blogs) => {
 	return blogs.reduce(likeReducer, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+	const maxLikes = Math.max.apply(
+		Math,
+		blogs.map((blog) => {
+			return blog.likes
+		})
+	)
+	return blogs.find((blog) => {
+		return blog.likes === maxLikes
+	})
+}
+
+const mostBLogs = (blogs) => {
+	const authorsCount = {}
+	let maxAuthor = { author: '', blogs: 0 }
+	blogs.forEach(blog => {
+		authorsCount[blog.author] ? authorsCount[blog.author] += 1 : authorsCount[blog.author] = 1
+
+		if (maxAuthor.blogs < authorsCount[blog.author]) {
+			maxAuthor = { author: blog.author, blogs: authorsCount[blog.author]}
+		}
+	});
+
+	return maxAuthor
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog }
