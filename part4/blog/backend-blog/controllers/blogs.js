880c40f7997c7949a13e0b7eb4fab3367512e170
RECturtle <spencer.mertes@gmail.com>
2023-02-12 15:18:54 -0500

    complete 4.15

--- part4/blog/backend-blog/controllers/blogs.js
+++ part4/blog/backend-blog/controllers/blogs.js
@@ -1,45 +1,62 @@
-const blogRouter = require("express").Router()
-const Blog = require("../models/blog")
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogRouter.get("/", async (request, response) => {
-	const blogs = await Blog.find({})
-	return response.json(blogs)
-})
+	const blogs = await Blog.find({});
+	return response.json(blogs);
+});
 
 blogRouter.post("/", async (request, response) => {
-	const blog = new Blog(request.body)
+	const body = request.body;
+	const user = await User.findById(body.userId);
+	const blog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes,
+		user: user.id,
+	});
+
 	if (blog.likes === undefined) {
-		blog.likes = 0
+		blog.likes = 0;
 	}
 
 	if (blog.title === undefined || blog.url === undefined) {
-		return response.status(400).end()
+		return response.status(400).end();
 	}
 
-	const savedBlog = await blog.save()
-	return response.status(201).json(savedBlog)
-})
+	const savedBlog = await blog.save();
+	user.blogs = user.blogs.concat(savedBlog._id);
+	await user.save();
+
+	return response.status(201).json(savedBlog);
+});
 
 blogRouter.delete("/:id", async (request, response) => {
-	await Blog.findByIdAndRemove(request.params.id)
-	response.status(204).end()
-})
+	await Blog.findByIdAndRemove(request.params.id);
+	response.status(204).end();
+});
 
-blogRouter.put("/:id", async (request,response) => {
+blogRouter.put("/:id", async (request, response) => {
 	try {
-		const body = request.body
+		const body = request.body;
 		const blog = {
 			title: body.title,
 			author: body.author,
 			url: body.url,
-			likes: body.likes
-		}
-	
-		const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-		response.json(updatedBlog)
-	} catch (exception) {
-		return exception
-	}
-})
+			likes: body.likes,
+		};
 
-module.exports = blogRouter
+		const updatedBlog = await Blog.findByIdAndUpdate(
+			request.params.id,
+			blog,
+			{ new: true }
+		);
+		response.json(updatedBlog);
+	} catch (exception) {
+		return exception;
+	}
+});
+
+module.exports = blogRouter;
