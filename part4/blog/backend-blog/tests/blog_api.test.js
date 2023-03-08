c1bca1b0974b4e744b1db54c7e82a6040ea032a5
RECturtle <spencer.mertes@gmail.com>
2023-03-08 07:30:07 -0500

    feat: finish up test creation

--- part4/blog/backend-blog/tests/blog_api.test.js
+++ part4/blog/backend-blog/tests/blog_api.test.js
@@ -13,124 +13,174 @@ beforeEach(async () => {
 	await User.deleteMany({});
 });
 
-test("blogs are returned as json", async () => {
-	await api
-		.get("/api/blogs")
-		.expect(200)
-		.expect("Content-Type", /application\/json/);
-});
+describe("Testing blog functionality", () => {
+	test("blogs are returned as json", async () => {
+		await api
+			.get("/api/blogs")
+			.expect(200)
+			.expect("Content-Type", /application\/json/);
+	});
 
-test("there are two blogs returned", async () => {
-	const response = await api.get("/api/blogs");
-	expect(response.body).toHaveLength(2);
-});
+	test("there are two blogs returned", async () => {
+		const response = await api.get("/api/blogs");
+		expect(response.body).toHaveLength(2);
+	});
 
-test("there is an id field returned", async () => {
-	const response = await api.get("/api/blogs");
-	expect(response.body[0].id).toBeDefined();
-});
+	test("there is an id field returned", async () => {
+		const response = await api.get("/api/blogs");
+		expect(response.body[0].id).toBeDefined();
+	});
 
-test("a new blog without a token fails", async () => {
-	const newBlog = {
-		title: "Holy Moly",
-		author: "Guacamole",
-		url: "google.com",
-		likes: 10,
-	};
+	test("a new blog without a jwt token fails", async () => {
+		const newBlog = {
+			title: "Holy Moly",
+			author: "Guacamole",
+			url: "google.com",
+			likes: 10,
+		};
 
-	await api.post("/api/blogs").send(newBlog).expect(401);
-});
+		await api.post("/api/blogs").send(newBlog).expect(401);
+	});
 
-test("a new blog gets added successfully", async () => {
-	await api.post("/api/users").send(helper.initialUser);
-	const user = {
-		username: "user",
-		password: "password",
-	};
+	test("a new blog gets added successfully", async () => {
+		await api.post("/api/users").send(helper.initialUser);
+		const user = {
+			username: "user",
+			password: "password",
+		};
 
-	const userLogin = await api.post("/api/login").send(user);
+		const userLogin = await api.post("/api/login").send(user);
 
-	const newBlog = {
-		title: "Holy Moly",
-		author: "Guacamole",
-		url: "google.com",
-		likes: 10,
-	};
+		const newBlog = {
+			title: "Holy Moly",
+			author: "Guacamole",
+			url: "google.com",
+			likes: 10,
+		};
 
-	// receive a successful response
-	const response = await api
-		.post("/api/blogs")
-		.set("Authorization", `Bearer ${userLogin.body.token}`)
-		.send(newBlog)
-		.expect(201);
+		// receive a successful response
+		const response = await api
+			.post("/api/blogs")
+			.set("Authorization", `Bearer ${userLogin.body.token}`)
+			.send(newBlog)
+			.expect(201);
 
-	// response contains title
-	expect(response.body.title).toBe("Holy Moly");
+		// response contains title
+		expect(response.body.title).toBe("Holy Moly");
 
-	// one more blog in db
-	const responseCount = await api.get("/api/blogs");
-	expect(responseCount.body).toHaveLength(3);
+		// one more blog in db
+		const responseCount = await api.get("/api/blogs");
+		expect(responseCount.body).toHaveLength(3);
 
-	// responses contain new blog
-	const blogs = responseCount.body.map((b) => b.title);
-	expect(blogs).toContain("Holy Moly");
-});
+		// responses contain new blog
+		const blogs = responseCount.body.map((b) => b.title);
+		expect(blogs).toContain("Holy Moly");
+	});
 
-test("a new blog excluding the likes field will return 0", async () => {
-	const newBlog = {
-		title: "Where are my likes?",
-		author: "Who Knows",
-		url: "nolikes.com",
-	};
+	test("a new blog excluding the likes field will return 0 likes", async () => {
+		await api.post("/api/users").send(helper.initialUser);
+		const user = {
+			username: "user",
+			password: "password",
+		};
 
-	const response = await api.post("/api/blogs").send(newBlog).expect(201);
+		const userLogin = await api.post("/api/login").send(user);
+		const newBlog = {
+			title: "Where are my likes?",
+			author: "Who Knows",
+			url: "nolikes.com",
+		};
 
-	expect(response.body.likes).toBe(0);
-});
+		const response = await api
+			.post("/api/blogs")
+			.set("Authorization", `Bearer ${userLogin.body.token}`)
+			.send(newBlog)
+			.expect(201);
 
-test("a new blog requires a title and url", async () => {
-	const noTitleBlog = {
-		author: "tester mcGee",
-		url: "google.com",
-		likes: 10,
-	};
+		expect(response.body.likes).toBe(0);
+	});
 
-	const noUrlBlog = {
-		author: "tester mcGee",
-		title: "Testing in Space",
-		likes: 100,
-	};
+	test("a new blog requires a title and url", async () => {
+		await api.post("/api/users").send(helper.initialUser);
+		const user = {
+			username: "user",
+			password: "password",
+		};
 
-	await api.post("/api/blogs").send(noTitleBlog).expect(400);
+		const userLogin = await api.post("/api/login").send(user);
 
-	await api.post("/api/blogs").send(noUrlBlog).expect(400);
-});
+		const noTitleBlog = {
+			author: "tester mcGee",
+			url: "google.com",
+			likes: 10,
+		};
 
-test("succeeds with status code 204 if id is valid", async () => {
-	const response = await api.get("/api/blogs");
-	const blogToDelete = response.body[0];
+		const noUrlBlog = {
+			author: "tester mcGee",
+			title: "Testing in Space",
+			likes: 100,
+		};
 
-	await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+		await api
+			.post("/api/blogs")
+			.set("Authorization", `Bearer ${userLogin.body.token}`)
+			.send(noTitleBlog)
+			.expect(400);
 
-	const remainingBlogs = await api.get("/api/blogs");
+		await api
+			.post("/api/blogs")
+			.set("Authorization", `Bearer ${userLogin.body.token}`)
+			.send(noUrlBlog)
+			.expect(400);
+	});
 
-	expect(remainingBlogs.body).toHaveLength(1);
+	test("Delete succeeds with status code 204 if id is valid", async () => {
+		await api.post("/api/users").send(helper.initialUser);
+		const user = {
+			username: "user",
+			password: "password",
+		};
 
-	const blogTitles = remainingBlogs.body.map((b) => b.title);
+		const userLogin = await api.post("/api/login").send(user);
 
-	expect(blogTitles).not.toContain(blogToDelete.title);
-});
+		const newBlog = {
+			title: "Holy Moly",
+			author: "Guacamole",
+			url: "google.com",
+			likes: 10,
+		};
 
-test("successfully update blogs", async () => {
-	const response = await api.get("/api/blogs");
-	const blogToUpdate = response.body[0];
-	blogToUpdate.likes = 10000;
+		// Create a blog with the user so we can delete it
+		const response = await api
+			.post("/api/blogs")
+			.set("Authorization", `Bearer ${userLogin.body.token}`)
+			.send(newBlog);
 
-	const updatedBlog = await api
-		.put(`/api/blogs/${blogToUpdate.id}`)
-		.send(blogToUpdate);
+		await api
+			.delete(`/api/blogs/${response.body.id}`)
+			.set("Authorization", `Bearer ${userLogin.body.token}`)
+			.expect(204);
 
-	expect(updatedBlog.body.likes).toBe(10000);
+		const remainingBlogs = await api.get("/api/blogs");
+
+		expect(remainingBlogs.body).toHaveLength(2);
+
+		const blogTitles = remainingBlogs.body.map((b) => b.title);
+
+		expect(blogTitles).not.toContain(response.body.title);
+	});
+
+	test("successfully update blogs", async () => {
+		const response = await api.get("/api/blogs");
+		const blogToUpdate = response.body[0];
+		blogToUpdate.likes = 10000;
+
+		const updatedBlog = await api
+			.put(`/api/blogs/${blogToUpdate.id}`)
+			.send(blogToUpdate);
+
+		expect(updatedBlog.body.likes).toBe(10000);
+	});
 });
 
 describe("Testing the users", () => {
